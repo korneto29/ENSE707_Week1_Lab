65ee25ac3d9140df10dc6016fb8ba9b963fa078a
korneto29 <arbee1129@gmail.com>
2026-07-15 16:09:43 +1200

    activity 2 and activity 3 done

---
+++ BankAccount.cs
@@ -0,0 +1,30 @@
+namespace ENSE707_Week1_Lab;
+
+public class BankAccount
+{
+    public string AccountHolder { get; }
+    
+    public decimal Balance { get; private set; }
+
+    public BankAccount(string accountHolder, decimal openingBalance)
+    {
+        AccountHolder = accountHolder;
+        Balance = openingBalance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+
+    public bool Withdraw(decimal amount)
+    {
+        Balance -= amount;
+        return true;
+    }
+
+    public decimal CalculateTransactionFee(decimal amount)
+    {
+        return amount * 0.02m;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ENSE707_Week1_Lab;
+
+BankAccount account = new BankAccount("Student User", 100);
+
+account.Deposit(50);
+account.Withdraw(200);
+
+Console.WriteLine($"Account Holder: {account.AccountHolder}");
+Console.WriteLine($"Current Balance: {account.Balance}");
+Console.WriteLine($"Fee on $100 transaction: {account.CalculateTransactionFee(100)}");
